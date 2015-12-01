c71973f42c21c1f6df415ffbec4e76a40f241338
aaron-hammond <aaron.hammond@live.com>
2015-12-01 17:53:13 +0000

    initial code

---
+++ Ambiguous.Reference.Test/UnitTest.cs
@@ -0,0 +1,12 @@
+﻿namespace Ambiguous.Reference.Test
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class UnitTest
+    {
+        public void Given()
+        {
+            var controller = new ValuesController();
+        }
+    }
+}
